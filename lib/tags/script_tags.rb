be10eab459fc414880249a8d6b9536fc4d0c61a7
John Mortlock <john.mortlock@gmail.com>
2017-07-11 16:41:30 +0930

    Add a new tag for loading javascript bundles

--- lib/tags/script_tags.rb
+++ lib/tags/script_tags.rb
@@ -7,4 +7,14 @@ module ScriptTags
     query = "?#{CGI::escape(version.value)}"
     raw %{<script src="/ecom-engine-js/#{tag.attr['src']}#{query}"></script>}
   end
+
+  desc 'Ecom-Engine javascript bundle'
+  tag 'ee_bundle_tag' do |tag|
+    bundle_url = PageMountCalculator.new(@request).ecom_engine_url + "/app_cells?name=header/#{tag.attr['name']}"
+    uri = URI.parse(bundle_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+    response = http.get uri.request_uri
+    raw response.body.strip
+  end
 end
